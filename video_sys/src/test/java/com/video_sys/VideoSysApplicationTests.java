b133c6fd9dbc610abb0ff01c20b28db1a1905e36
Yuqing Liu <2271743507@qq.com>
2025-12-04 19:04:35 +0800

    Add files via upload

---
+++ video_sys/src/test/java/com/video_sys/VideoSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
